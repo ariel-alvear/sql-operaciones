deb4f91b2b841d86803835df03dad80f1d1c11f3
Ariel Alvear <ariel.alvear.l@gmail.com>
2020-11-10 21:12:27 -0300

    codigo corregido

--- desafio-operaciones.sql
+++ desafio-operaciones.sql
@@ -17,17 +17,17 @@ INSERT INTO post (id, username, creation_date, content, post_description) VALUES
 ALTER TABLE post
 ADD title VARCHAR(20);
 
-INSERT INTO post (title) VALUES Lorem ipsum1 WHERE id=1;
+INSERT INTO post (title) VALUES ('Lorem ipsum1') WHERE id=1;
 
-INSERT INTO post (title) VALUES Lorem ipsum2 WHERE id=2;
+INSERT INTO post (title) VALUES ('Lorem ipsum2') WHERE id=2;
 
-INSERT INTO post (title) VALUES Lorem ipsum3 WHERE id=3;
+INSERT INTO post (title) VALUES ('Lorem ipsum3') WHERE id=3;
 
 INSERT INTO post (id, username, creation_date, content, post_description) VALUES (4, 'Pedro', 'November 10, 2020', 'LOREM IPSUM', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.');
 
 INSERT INTO post (id, username, creation_date, content, post_description) VALUES (5, 'Pedro', 'November 10, 2020', 'LOREM IPSUM', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.');
 
-DELETE FROM post WHERE id=3
+DELETE FROM post WHERE id=3;
 
 INSERT INTO post (id, username, creation_date, content, post_description) VALUES (3, 'Carlos', 'November 10, 2020', 'LOREM IPSUM', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.');
 
