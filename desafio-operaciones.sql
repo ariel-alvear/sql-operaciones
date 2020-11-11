1d2b8b8eeb864e7be120f9bd98475cdc9f26c97f
Ariel Alvear <ariel.alvear.l@gmail.com>
2020-11-10 21:24:19 -0300

    parte 1 lista

--- desafio-operaciones.sql
+++ desafio-operaciones.sql
@@ -5,7 +5,8 @@ CREATE TABLE post(
     username VARCHAR(30),
     creation_date DATE,
     content VARCHAR(250),
-    post_description VARCHAR(250)
+    post_description VARCHAR(250),
+    PRIMARY KEY (id)
 );
 
 INSERT INTO post (id, username, creation_date, content, post_description) VALUES (1, 'Pamela', 'November 10, 2020', 'LOREM IPSUM', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.');
@@ -17,11 +18,11 @@ INSERT INTO post (id, username, creation_date, content, post_description) VALUES
 ALTER TABLE post
 ADD title VARCHAR(20);
 
-INSERT INTO post (title) VALUES ('Lorem ipsum1') WHERE id=1;
+UPDATE post title SET title='Lorem ipsum1' WHERE id=1;
 
-INSERT INTO post (title) VALUES ('Lorem ipsum2') WHERE id=2;
+UPDATE post title SET title='Lorem ipsum2' WHERE id=2;
 
-INSERT INTO post (title) VALUES ('Lorem ipsum3') WHERE id=3;
+UPDATE post title SET title='Lorem ipsum3' WHERE id=3;
 
 INSERT INTO post (id, username, creation_date, content, post_description) VALUES (4, 'Pedro', 'November 10, 2020', 'LOREM IPSUM', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.');
 
@@ -29,7 +30,6 @@ INSERT INTO post (id, username, creation_date, content, post_description) VALUES
 
 DELETE FROM post WHERE id=3;
 
-INSERT INTO post (id, username, creation_date, content, post_description) VALUES (3, 'Carlos', 'November 10, 2020', 'LOREM IPSUM', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.');
-
+INSERT INTO post (id, username, creation_date, content, post_description) VALUES (6, 'Carlos', 'November 10, 2020', 'LOREM IPSUM', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.');
 
 
